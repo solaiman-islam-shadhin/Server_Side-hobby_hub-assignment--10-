0c7c9fd88b3c014dbcf80732635ab9bd35df324a
solaiman-islam-shadhin <solaimanislamshadhin123@gmail.com>
2025-10-04 21:32:36 +0600

    mongo db connected

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,6 +15,35 @@ app.get("/", (req, res) => {
     res.send("Assignment 10 server is running");
 }
 );
+
+
+// URL encode the password to handle special characters
+const encodedPassword = encodeURIComponent(process.env.DB_PASSWORD);
+const uri = `mongodb+srv://${process.env.DB_USER}:${encodedPassword}@assignmetn-10.is3vjll.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+run();
+
+
 app.listen(port, () => {
     console.log(`Assignment 10 server is running on port: ${port}`);
 });
